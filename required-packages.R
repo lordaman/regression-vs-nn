9d922c520e0f08c2d1b661b3ded5ed80d03f2575
lordaman <amankothari042000gmail.com>
2021-12-24 05:15:11 -0500

    all files

---
+++ required-packages.R
@@ -0,0 +1,21 @@
+## If a package is installed, it will be loaded. If any 
+## are not, the missing package(s) will be installed 
+## from CRAN and then loaded.
+
+## First specify the packages of interest
+packages = c("tidyverse", "devtools",
+             "pROC", "MASS", "keras")
+
+## Now load or install&load all
+package.check <- lapply(
+  packages,
+  FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+      library(x, character.only = TRUE)
+    }
+  }
+)
+
+library(devtools)
+install_github("vqv/ggbiplot")
